45f399b989da4eeb2874a0f41b99ba04adffd93f
cseshohag <solaiman.shohag95@gmail.com>
2019-04-09 12:57:57 +0600

    Upload project

---
+++ ChattingApp/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChattingApp
+{
+    public class ChatHub : Hub
+    {
+        //public void Hello()
+        //{
+        //    Clients.All.hello();
+        //}
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            Clients.All.user(Context.User.Identity.Name);
+            return base.OnConnected();
+        }
+
+        public void send(string message)
+        {
+            Clients.Caller.message("You: " + message);
+            Clients.Others.message(Context.User.Identity.Name + ": " + message);
+        }
+    }
+}
